3149996dff0b4a4e63de91e37c19e9a663f34f4e
Bibash Bayalkoti <bayalkotibibash@gmail.com>
2026-01-29 16:53:25 +0545

    Try some templates

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('teacher/dashboard', teacher_dashboard, name='teacher_dashboard'),
+    path('teacher/mark', mark_attendance, name='mark_attendance'),
+    path('teacher/groups', groups, name='groups'),
+    path('teacher/attendance', qr_attendance, name='qr-attendance'),
+    path('teacher/reports', reports, name='reports')
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def teacher_dashboard(request):
+    return render(request, 'teacher_dashboard.html')
+
+def mark_attendance(request):
+    return render(request, 'mark_attendance.html')
+
+def groups(request):
+    return render(request, 'groups.html')
+
+def qr_attendance(request):
+    return render(request, 'qr-attendance.html')
+
+def reports(request):
+    return render(request, 'reports.html')
